8a1a7cc11bec559e9f1c853eb5c6dbf2e5450552
ahamemd.webdesigner@gmail.com <ahamemd.webdesigner@gmail.com>
2020-11-30 14:04:33 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  //console.log(__dirname);
+
+
+  res.render('index', { title: 'Arshiya' });
+});
+
+
+
+
+router.get('/createfile',(req,res)=>{
+
+  var hellow = fs.createWriteStream("hellow.txt",{flags:'a'}); //1)creates file
+  hellow.write("Hi, Mustaq how are your doing Users. \r\n");
+  hellow.write("Hi, Arshiya how are your doing Users. \r\n");
+  hellow.close();
+  res.send("file created");
+});
+
+
+
+router.get('/readfile',(req,res)=>{
+    fs.readFile('hellow.txt', 'utf8',(err, data)=> res.send(data) )
+});
+
+
+
+router.get('/deletfile',(req,res)=>{
+  fs.unlink("hellow.txt",()=>{});
+
+
+  res.send("file deleted");
+});
+
+
+
+
+module.exports = router;
