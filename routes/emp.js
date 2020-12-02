4e3cb246f52b4b9082c64990c54fde96cb0250cc
ahamemd.webdesigner@gmail.com <ahamemd.webdesigner@gmail.com>
2020-12-02 16:14:28 +0530

    saving post data to file

--- routes/emp.js
+++ routes/emp.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 
-router.get('/welcome', function(req, res, next) {
-    res.send("welcome to employ module");
+router.get('/', function(req, res, next) {
+    // res.send("welcome to employ module");
+    res.render('empform', { title: 'Arshiya' });
   });
   
+
+  router.post('/', function(req, res, next) {
+    // console.log(req.body);
+    var EmpFile = fs.createWriteStream(__dirname+"/../temp/empdata.txt",{flags:'a'});
+    EmpFile.write(req.body.empname);
+    EmpFile.write('\r\n');
+
+    EmpFile.close();
+
+
+    res.send("Empdata saved");
+  });
+
   module.exports = router;
